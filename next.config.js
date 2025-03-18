099b43af6997ab76ab79168e59c498aab33eac75
tural-musab <tural.musab.osmanli@gmail.com>
2025-03-18 22:33:01 +0400

    next.config.js dosyası eklendi

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  // Swagger UI bileşeni için gerekli olan transpile seçeneği
+  // transpilePackages: ['swagger-ui-react'],
+  // Vercel'de dağıtım sorunları nedeniyle standalone modunu devre dışı bırakıyoruz
+  // output: 'standalone',
+  serverExternalPackages: [],
+};
+
+module.exports = nextConfig; 
